12da05ee06c0d8976560ccfc91948ab067fb5fc9
Edson Valenzuela <edsonvalher@gmail.com>
2021-05-31 14:53:53 -0600

    primera carga

---
+++ src/main/java/com/udemy/controller/ExampleController.java
@@ -0,0 +1,32 @@
+package com.udemy.controller;
+
+import com.udemy.model.Person;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/example")
+public class ExampleController {
+
+	public static String EXAMPLE_VIEW = "example";
+
+	@GetMapping("/exampleString")
+	public String ExampleString(Model model) {
+		int i = 6 / 0;
+		model.addAttribute("person", new Person("Jon", 23));
+		return EXAMPLE_VIEW;
+	}
+
+	@GetMapping("/exampleMAV")
+	public ModelAndView exampleMAV() {
+		ModelAndView mav = new ModelAndView(EXAMPLE_VIEW);
+		int i = 6 / 0;
+		mav.addObject("person", new Person("Mikel", 30));
+		return mav;
+	}
+
+}
---
+++ src/main/java/com/udemy/model/Person.java
@@ -0,0 +1,41 @@
+package com.udemy.model;
+
+public class Person {
+    private String name;
+    private int age;
+
+    /**
+     * @return String return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return int return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person(String name, int age) {
+        super();
+        this.age = age;
+        this.name = name;
+    }
+
+}
