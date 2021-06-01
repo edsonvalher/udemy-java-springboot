50fa2ef6f620db8bf15051ab002bfa0a8cfbc97c
Edson Valenzuela <edsonvalher@gmail.com>
2021-05-31 18:35:45 -0600

    combinaciones de JPA

--- src/main/java/com/udemy/controller/components/ExampleComponents.java
+++ src/main/java/com/udemy/controller/components/ExampleComponents.java
@@ -1,13 +1,21 @@
 package com.udemy.controller.components;
 
+import com.udemy.repository.CourseJPARepository;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("exampleComponent")
 public class ExampleComponents {
 
     private static final Log LOG = LogFactory.getLog(ExampleComponents.class);
+
+    @Autowired
+    @Qualifier("courseJPARepository")
+    private CourseJPARepository courseJPARepository;
 
     public void sayHello() {
 
