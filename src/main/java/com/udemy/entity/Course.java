9767932db96a1b2c853b3cc2683fd5fc49b507fe
Edson Valenzuela <edsonvalher@gmail.com>
2021-05-31 18:26:01 -0600

    se agrego las dependencias en el POM y se creo la primera entidad llamada 'Course' tomar en cuenta   ddl-auto: update se actualizan los nombres de la tabla, esta crea la tabla sin necesidad de diseñarla en db

---
+++ src/main/java/com/udemy/entity/Course.java
@@ -0,0 +1,114 @@
+package com.udemy.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "course")
+
+public class Course {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "Id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private int price;
+
+    @Column(name = "hours")
+    private int hours;
+
+    public Course() {
+
+    }
+
+    public Course(int id, String name, String description, int price, int hours) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.hours = hours;
+    }
+
+    /**
+     * @return int return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return String return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return int return the price
+     */
+    public int getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    /**
+     * @return int return the hours
+     */
+    public int getHours() {
+        return hours;
+    }
+
+    /**
+     * @param hours the hours to set
+     */
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+}
